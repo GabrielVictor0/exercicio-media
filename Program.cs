e479bc860b13ba0e58bd4845ae94e709ac1bdcd0
Gabriel-Victor-Almeida <bielgvsa@gmail.com>
2023-03-29 10:49:17 -0300

    Exercício de média em C#

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine($"Insira sua nota1: ");
+float nota1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Insira sua nota2: ");
+float nota2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Insira sua nota3: ");
+float nota3 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Insira sua nota4: ");
+float nota4 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Insira sua nota5: ");
+float nota5 = float.Parse(Console.ReadLine());
+
+float media = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+
+Console.WriteLine($"Sua media de nota foi: {media}");
+
+
+
+
+
+
+
